568f1e879f8b9861910b0b9f88576ebe31c30dde
Stefano-Mazziotta <mazziottastefano@gmail.com>
2021-08-06 11:38:38 -0300

    v1.0 steff-analog_website - finished home page

---
+++ js/popup.js
@@ -0,0 +1,34 @@
+// Accedo a cada elemento necesario a modificar.
+var open_btn = document.getElementById('js-open-btn'),
+    overlay = document.getElementById('js-overlay'),
+    popup = document.getElementById('js-popup'),
+    close_btn = document.getElementById('js-close-btn');
+
+/* === al hacer click sobre el icon mail abre el popup ===*/
+open_btn.addEventListener('click', ()=>{
+    overlay.classList.add('active');
+    popup.classList.add('active');
+});
+
+/* === al hacer click sobre la cruz del popup, oculta el contenido. ===*/
+close_btn.addEventListener('click', ()=>{
+    // overlay.className = 'overlay';
+    // popup.className = 'popup-container';
+    overlay.classList.remove('active');
+    popup.classList.remove('active');
+
+})
+
+/* === Si das click en el popup no lo cierra pero al hacer click sobre el overlay, oculta el contenido. ===*/
+overlay.addEventListener('click', () => {
+    
+    if (event.target.closest('.popup-container')){
+        return
+    }
+    else {
+        overlay.classList.remove('active');
+        popup.classList.remove('active');
+    }
+})
+
+
--- js/prueba.js
+++
@@ -1,1 +0,0 @@
-console.log({"Apellido":"Mazziotta","Edad":18})
