0a0050ee40070385ba0a9cf1efb8002c71634e21
Stefano-Mazziotta <mazziottastefano@gmail.com>
2021-09-03 12:40:53 -0300

    random-photo optimization - stable v0.6

--- js/random-photo.js
+++ js/random-photo.js
@@ -3,9 +3,8 @@
 rndm_vertical_or_horizontal() -> String
 Devuelve "vertical" u "horizontal" aleatoriamente.   */
 function rndm_vertical_or_horizontal(){
-    const   MIN = 1,
-            MAX = 2;
-            CONTAINER_IMG = document.getElementById('--js_fix-width');
+    const MIN = 1;
+    const MAX = 2;
     let randomNumber;
 
     randomNumber = Math.floor(Math.random() * ((MAX + 1) - MIN) + MIN);
@@ -16,39 +15,6 @@ function rndm_vertical_or_horizontal(){
         return 'Vertical';
     }
 };
-/*
-responsive_width(orientation) -> DOM - width size responsive
-Modifica el width del contenedor de la imágen según la pantalla del dispositivo. */
-function responsive_width(orientation){
-    MAX_WIDTH = 840;
-    MIN_WIDTH = 500;
-    let widthWindow;
-    widthWindow = screen.width;
-
-    if (orientation == "Vertical"){
-        if (widthWindow < MIN_WIDTH){
-            CONTAINER_IMG.removeAttribute("style");
-            CONTAINER_IMG.style.width = '100%';
-
-        } else if (widthWindow >= MIN_WIDTH && widthWindow <= MAX_WIDTH){
-            CONTAINER_IMG.removeAttribute("style");
-            CONTAINER_IMG.style.width = '65%';
-
-        } else {
-            CONTAINER_IMG.removeAttribute("style");
-            CONTAINER_IMG.style.width = '30%';
-        }
-    } else if (orientation == "Horizontal"){
-        if (widthWindow <= MAX_WIDTH){
-            CONTAINER_IMG.removeAttribute("style");
-            CONTAINER_IMG.style.width = '100%';
-        } else {
-            
-            CONTAINER_IMG.style.width = '70%';
-        }
-    }
-}
-
 
 /*
 rndm_generate() -> number
@@ -68,49 +34,92 @@ repeat_validator() -> string
 - Si el new_src ya se encuentra en SRC html, obtiene un nuevo src.   
 - Caso contrario devuelve el new_src   */
 function repeat_validator(new_src) {
-    let startURL, endURL, substrURL;
-    const   IMAGE = document.getElementById('--js_random__img-id'),
-            OLD_SRC = IMAGE.src;
-    
-    // Obtengo el src que necesito para la verificación.
-    startURL = OLD_SRC.lastIndexOf("/components") + 1;
-    endURL = OLD_SRC.length;
-    substrURL = OLD_SRC.substring(startURL,endURL);
+    const IMAGE = document.getElementById('--js_random__img-id');
+    const OLD_SRC = IMAGE.attributes[1]; // obtengo el src actual del sitio.
     
     // Si new_src es igual al HTML, devuelve un nuevo src.
     // Si no, devuelve el src que se creó inicialmente.
-    if (substrURL == new_src) {
-        while (substrURL == new_src){
+    if (OLD_SRC == new_src) {
+        while (OLD_SRC == new_src){
             let orientation = rndm_vertical_or_horizontal();
             let randomNum = rndm_generate();
             new_src = `components/images/random_photo/${orientation}/${randomNum}.jpg`;
         }
         return new_src;
+
     } else {
         return new_src;
     }
 }
 
+/*  responsiveHorizontal()
+    Modifica el width del contenedor img.
+    Adapta la imágen HORIZONTAL según el tamaño del dispositivo.
+*/
+function responsive_horizontal(){
+    const LAPTOP = "1024px";
+    let breakpoint = matchMedia(`(max-width:${LAPTOP})`);
+    
+    if(breakpoint.matches){
+        CONTAINER_IMG.style.setProperty('width', "100%");
+    } else{
+        CONTAINER_IMG.style.setProperty('width', "70%");
+    }
+}
+
+/*  responsiveVertical()
+    Modifica el width del contenedor img.
+    Adapta la imágen VERTICAL según el tamaño del dispositivo.
+*/
+function responsive_vertical(){
+    const mobile = "425px";
+    const tablet = "768px";
+    const laptop = "1024px";
+    const laptopL = "1440px";
+    const desktopXL = "2560px";
+
+    let breakpoint_mobile = matchMedia(`(max-width:${mobile})`);
+    let breakpoint_tablet = matchMedia(`(max-width:${tablet})`);
+    let breakpoint_laptop = matchMedia(`(max-width:${laptop})`);
+    let breakpoint_laptopL = matchMedia(`(max-width:${laptopL})`);
+    let breakpoint_desktopXL = matchMedia(`(max-width:${desktopXL})`);
+    
+
+    if(breakpoint_mobile.matches){
+        CONTAINER_IMG.style.setProperty('width', "100%");
+    } else if (breakpoint_tablet.matches){
+        CONTAINER_IMG.style.setProperty('width', "70%");
+    } else if (breakpoint_laptop.matches){
+        CONTAINER_IMG.style.setProperty('width', "50%")
+    } else if(breakpoint_laptopL.matches){
+        CONTAINER_IMG.style.setProperty('width', "35%")
+    } else if(breakpoint_desktopXL.matches) {
+        CONTAINER_IMG.style.setProperty('width', "30%")
+    }
+}
 /* --------- ✓ Functions ✓ --------- */
 
 /* --------- script --------- */
-window.addEventListener('load', ()=>{
-    // accedo a los elementos botón e imágen.
-    const   BUTTON = document.getElementById('--js_random__btn-id'),
-            IMAGE = document.getElementById('--js_random__img-id');
-    
-    // cuando se haga click en el botón cambiar src de la imágen.
-    BUTTON.onclick = () => {
-        let orientation = rndm_vertical_or_horizontal();
-        let randomNum = rndm_generate();
-        // Creo new_src a partir de los valores random.
-        new_src = `components/images/random_photo/${orientation}/${randomNum}.jpg`;
-        // Verifico si new_src está en src HTML.
-        new_src = repeat_validator(new_src);
-        IMAGE.src = new_src;
-        // Modifico el width de la image según widthscreen.
-        responsive_width(orientation);
-    };
+// accedo a los elementos botón e imágen.
+const BUTTON = document.getElementById('--js_random__btn-id');
+const CONTAINER_IMG = document.getElementById('--js_random__img-container');
+const IMAGE = document.getElementById('--js_random__img-id');
+
+// cuando se haga click en el botón cambiar src de la imágen.
+BUTTON.addEventListener('click', () => {
+    let orientation = rndm_vertical_or_horizontal();
+    let randomNum = rndm_generate();
+
+    // Creo new_src a partir de los valores random.
+    new_src = `components/images/random_photo/${orientation}/${randomNum}.jpg`;
+    // Verifico si new_src está en src HTML.
+    new_src = repeat_validator(new_src); 
+    // Si la orientación es horizontal, llamo a la función responsiveHorizontal().
+    // Si la orientación es vertical, llamo a la función responsiveVertical().
+    IMAGE.setAttribute("src",new_src);
+    if (orientation == "Horizontal") {
+        responsive_horizontal();
+    } else {
+        responsive_vertical();
+    }
 });
-
-
